1dc79abe39ce36194ded5561bbbf739b662d3ef0
Timothy Keyes <tkeyes@stanford.edu>
2021-08-04 11:35:12 -0700

    delete deprecated functions.

--- R/visualization.R
+++ R/visualization.R
@@ -1,3 +1,30 @@
+
+
+#### geom_tof_ridges.R ####
+# Description: A function that encodes a geom specifically for generating univariate histograms
+#              of CyTOF data.
+#             Currently VERY EXPERIMENTAL and NOT OPTIMIZED
+#
+# Inputs:
+#     - rel_min_height, size, and scale = arguments to pass to geom_density_ridges_gradient()
+#                                         (with nice defaults set)
+#     - ... = additional arguments to pass to geom_density_ridges_gradient()
+#
+# Outputs: None
+#
+# Dependencies:
+#     - ggridges package and all of its dependencies
+
+geom_tof_ridges <- function(rel_min_height = 0.00, size = 0.5, scale = 1.4, ...) {
+  geom_density_ridges_gradient(
+    rel_min_height = rel_min_height,
+    size = size,
+    scale = scale,
+    ...
+  )
+}
+
+
 ######################   tof_histogram
 
 #' Generate univariate histograms of CyTOF data from a `tof_tibble`.
